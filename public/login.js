9fb62a376ae7c14e7f728092d5b918af2ebe78c6
Derek Cheng <derekcheng33@gmail.com>
2023-12-22 11:38:38 +0800

    signup and login completed

---
+++ public/login.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+// Serve static files (e.g., HTML, CSS) from the "public" folder
+app.use(express.static('public'));
+
+// Parse incoming requests with JSON payloads
+app.use(bodyParser.json());
+
+// Parse incoming requests with urlencoded payloads
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve the HTML file
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/login.html');
+});
+
+// Handle form submission
+app.post('/submit', (req, res) => {
+  // Access form data from req.body
+  const { email, password } = req.body;
+
+  // Process the form data (e.g., store in a database, perform authentication)
+  console.log('Form submitted with email:', email, 'and password:', password);
+
+  // Send a response to the client
+  res.send('Form submitted successfully!');
+});
+
+app.post('/submitSignup', (req, res) => {
+  // Access form data from req.body
+  const { email, password } = req.body;
+
+  // Process the form data (e.g., store in a database, perform authentication)
+  console.log('Form submitted with email:', email, 'and password:', password);
+
+  // Send a response to the client
+  res.send('Form submitted successfully!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+function forgotPassword() {
+  // implement forgotPassword
+}
+
+
+function switchToSignup() {
+    window.location.href="signup.html";
+}
+
+function switchToSignin() {
+  window.location.href="login.html"
+}
