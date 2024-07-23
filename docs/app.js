6a5cf5a8a420b7cfe10500961cf4a103d96d2f2e
Derek Cheng <derekcheng33@gmail.com>
2024-07-23 11:13:34 +0800

    removed a line that is conflicting (package)

--- docs/app.js
+++ docs/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { MongoClient } = require('mongodb');
-const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const path = require('path');
 
